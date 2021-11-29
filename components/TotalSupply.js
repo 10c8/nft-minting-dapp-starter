662bd33e052fd6975749a1167301c315238ccffd
10c8 <2660859+10c8@users.noreply.github.com>
2021-11-29 18:33:03 +0000

    First main change

--- components/TotalSupply.js
+++ components/TotalSupply.js
@@ -1,67 +1,119 @@
-import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
-import { hasEthereum, requestAccount } from '../utils/ethereum'
-import Minter from '../src/artifacts/contracts/Minter.sol/Minter.json'
+import { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+
+import { hasEthereum, requestAccount } from '../utils/ethereum';
+import Minter from '../src/artifacts/contracts/Minter.sol/Minter.json';
 
 export default function TotalSupply() {
-    // UI state
-    const [loading, setLoading] = useState(true)
-    const [totalMinted, setTotalMinted] = useState(0)
-    const [totalValue, setTotalValue] = useState(0)
+  // UI state
+  const [loading, setLoading] = useState(true);
+  const [totalMinted, setTotalMinted] = useState(0);
+  const [totalValue, setTotalValue] = useState(0);
 
-    // Constants
-    const TOTAL = 10000;
+  // Constants
+  const TOTAL = 10000;
 
-    useEffect( function() {
-        async function fetchTotals() {
-            if(! hasEthereum()) {
-                console.log('Install MetaMask')
-                setLoading(false)
-                return
-            }
-    
-            await getTotalSupply()
-            await getTotalValue()
-        
-            setLoading(false)
-        }
-        fetchTotals();
-    });
+  useEffect(() => {
+    async function fetchTotals() {
+      if (!hasEthereum()) {
+        console.log('Install MetaMask');
+        setLoading(false);
+        return;
+      }
 
-    // Get total supply of tokens from smart contract
-    async function getTotalSupply() {
-        try {
-          // Interact with contract
-          const provider = new ethers.providers.Web3Provider(window.ethereum)
-          const contract = new ethers.Contract(process.env.NEXT_PUBLIC_MINTER_ADDRESS, Minter.abi, provider)
-          const data = await contract.totalSupply()
-      
-          setTotalMinted(data.toNumber());
-        } catch(error) {
-            console.log(error)
-        }
+      await getTotalSupply();
+      await getTotalValue();
+  
+      setLoading(false);
     }
 
-     // Get total value collected by the smart contract
-     async function getTotalValue() {
-        try {
-          // Interact with contract
-          const provider = new ethers.providers.Web3Provider(window.ethereum)
-          const contract = new ethers.Contract(process.env.NEXT_PUBLIC_MINTER_ADDRESS, Minter.abi, provider)
-          const data = await contract.getBalance()
-      
-          setTotalValue(ethers.utils.formatEther(data).toString());
-        } catch(error) {
-            console.log(error)
-        }
-    }
+    fetchTotals();
+  });
 
-    return (
-        <>
-            <p>
-                Tokens minted: { loading ? 'Loading...' : `${totalMinted}/${TOTAL}` }<br />
-                Contract value: { loading ? 'Loading...' : `${totalValue}ETH` }
-            </p>
-        </>
-    )
+  // Get total supply of tokens from smart contract
+  async function getTotalSupply() {
+    try {
+      // Interact with contract
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(
+        process.env.NEXT_PUBLIC_MINTER_ADDRESS,
+        Minter.abi,
+        provider
+      );
+      const data = await contract.totalSupply();
+  
+      setTotalMinted(data.toNumber());
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  // Get total value collected by the smart contract
+  async function getTotalValue() {
+    try {
+      // Interact with contract
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(
+        process.env.NEXT_PUBLIC_MINTER_ADDRESS,
+        Minter.abi,
+        provider
+      );
+      const data = await contract.getBalance();
+  
+      setTotalValue(ethers.utils.formatEther(data).toString());
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className="flex flex-col gap-3 items-center w-full">
+      {/* Progress Bar */}
+      <div
+        className="grid overflow-hidden relative grid-cols-5 w-full h-12 text-xs border-4 border-black"
+      >
+        <div
+          className="absolute top-0 left-0 h-full bg-red"
+          style={{
+            width: `${100 * (totalMinted / TOTAL)}%`
+          }}
+        />
+        <span
+          className="flex col-span-1 justify-center h-full border-r border-gray"
+          style={{
+            lineHeight: '46px'
+          }}
+        >
+          GEN 0
+        </span>
+        <span
+          className="flex col-span-1 justify-center h-full border-r border-l border-gray"
+          style={{
+            lineHeight: '46px'
+          }}
+        >
+          200 ETH
+        </span>
+        <span
+          className="flex col-span-2 justify-center h-full border-r border-l border-gray"
+          style={{
+            lineHeight: '46px'
+          }}
+        >
+          400 ETH
+        </span>
+        <span
+          className="flex col-span-1 justify-center h-full border-l border-gray"
+          style={{
+            lineHeight: '46px'
+          }}
+        >
+          800 ETH
+        </span>
+      </div>
+
+      <span>{loading ? 'Loading...' : `${totalMinted}/${TOTAL}`} MINTED</span>
+      {/* <span>{loading ? 'Loading...' : `${totalValue}ETH`}</span> */}
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import Head from 'next/head'
-import { useState, useRef } from 'react'
-import { ethers } from 'ethers'
+import Head from 'next/head';
+import { useState, useRef } from 'react';
+import { ethers } from 'ethers';
+
 import { hasEthereum } from '../utils/ethereum'
-import Minter from '../src/artifacts/contracts/Minter.sol/Minter.json'
-import TotalSupply from '../components/TotalSupply'
-import Wallet from '../components/Wallet'
-import YourNFTs from '../components/YourNFTs'
+import TotalSupply from '../components/TotalSupply';
+import Wallet from '../components/Wallet';
+import YourNFTs from '../components/YourNFTs';
+import Minter from '../src/artifacts/contracts/Minter.sol/Minter.json';
 
 export default function Home() {
   // Constants
@@ -13,121 +14,169 @@ export default function Home() {
   const MAX_MINT = 10;
 
   // UI state
-  const [mintQuantity, setMintQuantity] = useState(1)
-  const mintQuantityInputRef = useRef()
-  const [mintError, setMintError] = useState(false)
-  const [mintMessage, setMintMessage] = useState('')
-  const [mintLoading, setMintLoading] = useState(false)
+  const [mintQuantity, setMintQuantity] = useState(1);
+  const mintQuantityInputRef = useRef();
+  const [mintError, setMintError] = useState(false);
+  const [mintMessage, setMintMessage] = useState('');
+  const [mintLoading, setMintLoading] = useState(false);
 
   // Call smart contract to mint NFT(s) from current address
   async function mintNFTs() {
     // Check quantity
-    if ( mintQuantity < 1 ) {
-      setMintMessage('You need to mint at least 1 NFT.')
-      setMintError(true)
-      mintQuantityInputRef.current.focus()
-      return
+    if (mintQuantity < 1) {
+      setMintMessage('You need to mint at least 1 NFT.');
+      setMintError(true);
+      mintQuantityInputRef.current.focus();
+      return;
     }
-    if ( mintQuantity > MAX_MINT ) {
-      setMintMessage('You can only mint a maximum of 10 NFTs.')
-      setMintError(true)
-      mintQuantityInputRef.current.focus()
-      return
+
+    if (mintQuantity > MAX_MINT) {
+      setMintMessage('You can only mint a maximum of 10 NFTs.');
+      setMintError(true);
+      mintQuantityInputRef.current.focus();
+      return;
     }
 
     // Get wallet details
-    if(!hasEthereum()) return
+    if(!hasEthereum())
+      return;
+    
     try {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner()
+      const signer = provider.getSigner();
 
       try {
-        const address = await signer.getAddress()
+        // const address = await signer.getAddress();
 
         setMintLoading(true);
-          // Interact with contract
-          const contract = new ethers.Contract(process.env.NEXT_PUBLIC_MINTER_ADDRESS, Minter.abi, signer)
-          const totalPrice = MINT_PRICE * mintQuantity
-          const transaction = await contract.mint(mintQuantity, { value: ethers.utils.parseEther(totalPrice.toString()) })
-          await transaction.wait()
 
-          mintQuantityInputRef.current.value = 0
-          setMintMessage(`Congrats, you minted ${mintQuantity} token(s)!`)
-          setMintError(false)
+        // Interact with contract
+        const contract = new ethers.Contract(
+          process.env.NEXT_PUBLIC_MINTER_ADDRESS,
+          Minter.abi,
+          signer
+        );
+        const totalPrice = MINT_PRICE * mintQuantity;
+        const transaction = await contract.mint(mintQuantity, {
+          value: ethers.utils.parseEther(totalPrice.toString())
+        });
+
+        await transaction.wait();
+
+        mintQuantityInputRef.current.value = 0;
+        setMintMessage(`Congrats, you minted ${mintQuantity} token(s)!`);
+        setMintError(false);
       } catch {
         setMintMessage('Connect your wallet first.');
-        setMintError(true)
+        setMintError(true);
       }
     } catch(error) {
-        setMintMessage(error.message)
-        setMintError(true)
+      setMintMessage(error.message);
+      setMintError(true);
     }
-    setMintLoading(false)
+
+    setMintLoading(false);
   }
 
   return (
-    <div className="max-w-xl mt-36 mx-auto px-4">
+    <div className="flex flex-col items-center p-8 mx-auto max-w-7xl">
       <Head>
         <title>NFT Minting dApp Starter</title>
         <meta name="description" content="Mint an NFT, or a number of NFTs, from the client-side." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Wallet />
-      <main className="space-y-8">
-        { ! process.env.NEXT_PUBLIC_MINTER_ADDRESS ? (
-            <p className="text-md">
-              Please add a value to the <pre>NEXT_PUBLIC_MINTER_ADDRESS</pre> environment variable.
-            </p>
-        ) : (
-          <>
-            <h1 className="text-4xl font-semibold mb-8">
-              NFT Minting dApp Starter
-            </h1>
-            <TotalSupply />
-            <div className="space-y-8">
-                <div className="bg-gray-100 p-4 lg:p-8">
-                  <div>
-                    <h2 className="text-2xl font-semibold mb-2">Mint NFTs</h2>
-                    <label className="text-gray-600 text-sm mb-2 inline-block">
-                      How many NFTs would you like to mint from the smart contract?
-                    </label>
-                    <div className="flex">
-                      <input
-                          className={ ! mintError ? "border p-4 text-center rounded-tl rounded-bl focus:outline-none focus:border-blue-600 w-2/3" : "border border-red-500 p-4 text-center rounded-tl rounded-bl focus:outline-none focus:border-blue-600 w-2/3"}
-                          onChange={ e => setMintQuantity(e.target.value)}
-                          value={mintQuantity}
-                          placeholder="1"
-                          type="number"
-                          min="1"
-                          max="10"
-                          ref={mintQuantityInputRef}
-                        />
-                      <button
-                        className="bg-blue-600 hover:bg-blue-700 text-white py-4 px-8 rounded-tr rounded-br w-1/3"
-                        onClick={mintNFTs}
-                      >
-                        { mintLoading ? 'Loading...' : 'Mint' }
-                      </button>
-                    </div>
-                    { mintMessage && <span className={mintError ? "text-red-600 text-xs mt-2 block" : "text-green-600 text-xs mt-2 block"}>{ mintMessage }</span> }
-                  </div>
-                </div>
-            </div>
-          </>
-        ) }
-        <YourNFTs />
+
+      <h1
+        className="text-6xl text-red-500"
+        style={{
+          fontFamily: 'Broken Console Bold',
+          textShadow: '6px 6px 0 black'
+        }}
+      >
+        NOT WOLF GAME
+      </h1>
+
+      <main
+        className="flex gap-6 items-center mt-6 w-full"
+        style={{
+          fontFamily: 'Broken Console Bold'
+        }}
+      >
+        {/* Minting Box */}
+        <div className="flex flex-col gap-8 items-center p-10 w-full border-4 border-black">
+          <h1 className="text-4xl">MINTING</h1>
+          <TotalSupply />
+          <button
+            className="px-8 pt-4 pb-3 bg-transparent border-4 border-black"
+            onClick={mintNFTs}
+          >
+            {mintLoading ? 'Loading...' : 'Mint'}
+          </button>
+        </div>
+
+        <div className="flex flex-col gap-8 items-center p-10 w-full h-60 border-4 border-black">
+          <Wallet />
+        </div>
       </main>
 
+      {/* <main className="space-y-8">
+        {!process.env.NEXT_PUBLIC_MINTER_ADDRESS ? (
+          <p className="text-md">
+            Please add a value to the <pre>NEXT_PUBLIC_MINTER_ADDRESS</pre> environment variable.
+          </p>
+        ) : (
+          <>
+            <h1 className="mb-8 text-4xl font-semibold">
+              NFT Minting dApp Starter
+            </h1>
+            <div className="space-y-8">
+              <div className="p-4 bg-gray-100 lg:p-8">
+                <div>
+                  <h2 className="mb-2 text-2xl font-semibold">Mint NFTs</h2>
+                  <label className="inline-block mb-2 text-sm text-gray-600">
+                    How many NFTs would you like to mint from the smart contract?
+                  </label>
+                  <div className="flex">
+                    <input
+                      className={!mintError
+                        ? "p-4 w-2/3 text-center rounded-tl rounded-bl border focus:outline-none focus:border-blue-600"
+                        : "p-4 w-2/3 text-center rounded-tl rounded-bl border border-red-500 focus:outline-none focus:border-blue-600"
+                      }
+                      onChange={e => setMintQuantity(e.target.value)}
+                      value={mintQuantity}
+                      placeholder="1"
+                      type="number"
+                      min="1"
+                      max="10"
+                      ref={mintQuantityInputRef}
+                    />
+                    <button
+                      className="px-8 py-4 w-1/3 text-white bg-blue-600 rounded-tr rounded-br hover:bg-blue-700"
+                      onClick={mintNFTs}
+                    >
+                      {mintLoading ? 'Loading...' : 'Mint'}
+                    </button>
+                  </div>
+                  {mintMessage && (
+                    <span
+                      className={mintError
+                        ? "block mt-2 text-xs text-red-600"
+                        : "block mt-2 text-xs text-green-600"
+                      }
+                    >
+                      {mintMessage}
+                    </span>
+                  )}
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+        <YourNFTs />
+      </main> */}
+
       <footer className="mt-20 text-center">
-        <a
-          href="https://github.com/tomhirst/solidity-nextjs-mint-starter/blob/main/README.md"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-600 hover:text-blue-700 mb-8 inline-block"
-        >
-          Read the docs
-        </a>
       </footer>
     </div>
-  )
-}
+  );
+};
